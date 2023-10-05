c7b3d61d21945552d583bda12d28c9c153dd6d66
mayonklepas <imonks21@gmail.com>
2023-10-05 17:28:34 +0700

    ada yang baru nih di little-maria framework lagi

--- controllers/Menu.php
+++ controllers/Menu.php
@@ -1,14 +1,49 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'] . "/helpers/DB.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/helpers/BaseFunction.php";
 
 class Menu extends BaseFunction {
+    
+    var $db;
+    
+    public function __construct() {
+        $this->db=new DB("webcpdb");
+    }
 
-    public function view($request) {
-        $menu= $this->db("webcpdb")->table("menu")->get();
+    public function view($req) {
+        $menu = $this->db->table("menu")->get();
+        
         $data=[
             "menu"=>$menu
         ];
         $this->viewPage("menu",$data);
+    }
+    
+    public function save($req) {
+        $data = [
+            "title"=>$req->title,
+            "synopsis"=>$req->synopsis,
+            "content"=>$req->content,
+            "banner"=>$req->banner,
+            "is_root"=>$req->isRoot,
+            "is_sub"=>$req->isSub,
+            "id_root"=>$req->idRoot
+        ];
+        
+        if($req->id==0){
+            $this->db->table("menu")->insert($data);
+        }else{
+            $data["id"]=$req->id;
+            $this->db->table("menu")->update($data);
+        }
+      
+        $this->redirect("/menu");
+        
+    }
+    
+    public function delete($req) {
+        $this->db->table("menu")->delete($req->id);
+        $this->redirect("/menu");
     }
 
 }
